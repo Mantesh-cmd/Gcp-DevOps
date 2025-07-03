bce8dcaf46b1f868fad258375aa856604c059dc7
biradam1 <mahantesh.biradar@contractors.roche.com>
2025-07-03 16:34:16 +0530

    cloudbuild file is updated

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from flask import Flask
+# from flask import Flask
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, Mahantesh how are you?!,your  application is running successfully now"
+# @app.route('/')
+# def home():
+#     return "Hello, Mahantesh how are you?!,your  application is running successfully now"
 
-if __name__ == '__main__':
-     app.run(host='0.0.0.0',port=8080)
+# if __name__ == '__main__':
+#      app.run(host='0.0.0.0',port=8080)
+
+def hello_world(request):
+    return "Hello from Cloud Function!"
