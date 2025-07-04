0c2bab7f334b6dbf7482ad258c567ae6d8bb0b23
biradam1 <mahantesh.biradar@contractors.roche.com>
2025-07-04 14:50:35 +0530

    changes

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-# from flask import Flask
+from flask import Flask
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# @app.route('/')
-# def home():
-#     return "Hello, Mahantesh how are you?!,your  application is running successfully now"
+@app.route('/')
+def home():
+    return "Hello, Mahantesh how are you?!,your  application is running successfully now"
 
-# if __name__ == '__main__':
-#      app.run(host='0.0.0.0',port=8080)
+if __name__ == '__main__':
+     app.run(host='0.0.0.0',port=8080)
 
-def test_function(request):
-    return "Hello from Cloud Function!"
+# def test_function(request):
+#     return "Hello from Cloud Function!"
