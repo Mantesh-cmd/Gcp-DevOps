cc3c354746a54e09114e16070d9256678f626698
mahantesh <dmahantesh777@gmail.com>
2025-07-13 11:54:25 +0000

    changes applied for cloudbuild file

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Mahantesh how are you?!,your  application is running successfully now"
+    return "You application has been deployed on GKE with version 1.0"
 
 if __name__ == '__main__':
      app.run(host='0.0.0.0',port=8080)
