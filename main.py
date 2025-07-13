4922df57dd5d94114887e7c366845d2d446de7a9
mahantesh <dmahantesh777@gmail.com>
2025-07-13 12:19:29 +0000

    new feature has been deployed

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "You application has been deployed on GKE with version 1.0"
+    return "You application has been deployed on GKE with version 2.0"
 
 if __name__ == '__main__':
      app.run(host='0.0.0.0',port=8080)
